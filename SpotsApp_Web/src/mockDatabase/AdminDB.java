63eaff11d46e909ddadea215debc56e01e338123
Utente <Utente@PCAlbi>
2022-06-18 12:12:31 +0200

    Completed MockDB for Users, Moderatori and Admin

--- SpotsApp_Web/src/mockDatabase/AdminDB.java
+++ SpotsApp_Web/src/mockDatabase/AdminDB.java
@@ -1,5 +1,7 @@
 package mockDatabase;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -11,8 +13,9 @@ public class AdminDB {
 	private Map<Admin, String> password;
 	
 	public AdminDB() {
-		init();
-		
+		this.admin = new ArrayList<>();
+		this.password = new HashMap<>();
+		init();		
 	}
 	
 	public List<Admin> getAdmin() {
@@ -31,9 +34,15 @@ public class AdminDB {
 		this.password = password;
 	}
 
-	private static void init() {
+	private void init() {
 		Admin a1 = new Admin("Alberto", "garagnanialberto@gmail.com");
 		Admin a2 = new Admin("Tommaso", "muzzitommaso@gmail.com");
-		Admin a3 = new Admin("Andrea", "ruggieriandrea@gmail.com");		
+		Admin a3 = new Admin("Andrea", "ruggieriandrea@gmail.com");
+		this.admin.add(a1);
+		this.admin.add(a2);
+		this.admin.add(a3);
+		this.password.put(a1, "Gara00");
+		this.password.put(a2, "Muztheoneandonly!");
+		this.password.put(a3, "RuggoTown");
 	}
 }
